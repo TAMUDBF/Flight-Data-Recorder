8555a96c1f14cd85ee522cabf4ec258f080ffb9e
Matthew <mthanjan@tamu.edu>
2026-03-18 10:03:24 -0500

    Update main.cpp for gps only

--- src/main.cpp
+++ src/main.cpp
@@ -7,35 +7,20 @@
 #include <config.h>
 
 Data data;
-imuHandler IMU;
 gpsHandler GPS;
-pitotHandler pitotTube;
-sbusHandler SBus;
-batteryHandler battery;
-ledHandler LED;
 loggingHandler logger;
 
 void setup(void) {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT); // Builtin LED
   Teensy3Clock.set(0);
-
-  LED.setup();
-  IMU.setup();
   GPS.setup();
-  pitotTube.setup();
-  SBus.setup();
-  battery.setup();
   logger.setup();
 }
 
 void loop() {
   delay(10); // read rate
-  IMU.read();
   GPS.read();
-  pitotTube.read();
-  SBus.read();
-  battery.read();
   logger.csvLog();
   logger.serialLog();
 }
